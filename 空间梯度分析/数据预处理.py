dfefa55c934ce3ee1a3d88b6b4f90de04af46d7c
Pengyu Chen <95490459+spacecow-boy@users.noreply.github.com>
2022-09-19 20:24:25 +0800

    Add files via upload

---
+++ 空间梯度分析/数据预处理.py
@@ -0,0 +1,12 @@
+import pandas as pd
+path = 'file.csv'
+
+# 使用pandas读入
+data = pd.read_csv(path)
+
+# 按列分离数据
+lng_o = data[['lng_o']]
+lat_o = data[['lat_o']]
+
+# 用zip()函数使两个list变成list of tuple
+zipped = zip()
