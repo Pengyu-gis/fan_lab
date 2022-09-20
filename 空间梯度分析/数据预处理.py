ec2fe2baec3a6afa14d725c100ec76ff2cb8d1bd
Pengyu Chen <95490459+spacecow-boy@users.noreply.github.com>
2022-09-20 08:56:54 +0800

    Update 数据预处理.py

--- 空间梯度分析/数据预处理.py
+++ 空间梯度分析/数据预处理.py
@@ -1,12 +1,17 @@
 import pandas as pd
 path = 'file.csv'
 
+
 # 使用pandas读入
 data = pd.read_csv(path)
 
 # 按列分离数据
-lng_o = data[['lng_o']]
-lat_o = data[['lat_o']]
+lng_o = data['lng_o']
+lat_o = data['lat_o']
+
+# print(lat_o,lng_o)
 
 # 用zip()函数使两个list变成list of tuple
-zipped = zip()
+zipped = zip(lng_o,lat_o)
+geolocations = list(zipped)
+print(geolocations)
